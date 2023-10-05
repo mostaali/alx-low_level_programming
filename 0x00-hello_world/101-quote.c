707307beae9a09fdb6c1903d9c052885b2995ee2
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-05 12:06:23 -0700

    done fast

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
  */
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, sizeof(message) - 1);
-return (1);
+    char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(2, str1, 59);
+    return (1);
 
 }
