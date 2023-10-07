4e844f6f92af24d8354b68e12a71c5dbc1cb0e29
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-07 04:16:10 -0700

    done fast

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
