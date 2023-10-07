c9c7a8d4f1d81e9fd08a1be0041e5e78e47f2a98
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-07 14:16:00 -0700

    done fast

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,12 @@ int main(void)
 	for (j = i + 1; j < 10; j++) 
 	{
 	putchar(i + '0');
+	putchar(j + '0');
+	if (i != 8)
+	{
 	putchar(',');
 	putchar(' ');
-	putchar(j + '0');
-	putchar(' ');
+	}
 	}
 	}
 
