d194242fa019aa8fc855b9c74f1503ab5e7992fc
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-07 04:39:39 -0700

    done fast

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i);
+	if (i != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
