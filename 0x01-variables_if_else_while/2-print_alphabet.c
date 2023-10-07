91430f9792a25d5010f98b14947e0d94e01a9364
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-07 02:53:36 -0700

    done fast

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		char ch;
-		for (ch = 'a'; ch <= 'z'; ch++) 
-		{
-			putchar(ch);
-		}
-		putchar('\n');
-		return (0);
+	putchar(ch);
 	}
+	putchar('\n');
+	return (0);
+}
