d07f7d3fd0d75e66c8f3559b5cb7b29df353b94d
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-10 20:42:47 -0700

    done fast

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int row, column, n;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			n = row * column;
+
+			if (column == 0)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (n < 10)
+				{
+					_putchar(' ')
+				}
+
+				_putchar(n / 10 + '0')
+				_putchar(n % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
