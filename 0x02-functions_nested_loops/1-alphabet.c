09dd148477b607c34357b03931200742dbc063e5
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-10 17:48:54 -0700

    done fast

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void _putchar(char c) {
+    write(1, &c, 1);
+}
+
+void print_alphabet(void) {
+    char letter = 'a';
+
+    while (letter <= 'z') {
+        _putchar(letter);
+        letter++;
+    }
+
+    _putchar('\n');
+}
