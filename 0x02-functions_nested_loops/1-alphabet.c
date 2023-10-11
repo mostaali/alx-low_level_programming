43292acf426eaad8ebb7caf8e997a14d397ce393
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-10 18:20:08 -0700

    done fast

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include "main.h"
 /**
-* main - check the code
+* alphabetfunction - check the code
 *
 * Return: Always 0.
 */
-void print_alphabet(void) /*alphabetfunction*/
+void print_alphabet(void)
 {
 	int a;
 
