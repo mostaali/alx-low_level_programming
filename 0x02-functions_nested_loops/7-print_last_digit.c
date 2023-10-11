ec078a5d9fb91a63ebbd05e6a3cb6279889404ba
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-10 21:46:09 -0700

    done fast

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,16 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = -n;
+		a = -1 * (n % 10);
 	}
+	else
+	{
 	a = n % 10;
+	}
+	if (a < 0)
+	{
+		a = -a;
+	}
 	 _putchar('0' + a);
 
 	return (a);
