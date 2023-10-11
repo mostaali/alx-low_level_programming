42e67e19b8bad484facaa581140e8974463f18eb
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-10 20:01:46 -0700

    done fast

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - check the code
+ * @n: our ain int
+ *
+ * Return: 1 if n is greater than zero, 0 if zero, -1 if less than zero
+ */
+
+int _abs(int n) 
+	{
+	int n;
+
+	if (n < 0) 
+	{
+	n= n*-1;
+	}
+	return (n);
+
+}
