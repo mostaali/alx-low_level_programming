741483474951d71ea55ad12975fcf4ff6f488906
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-10 19:01:52 -0700

    done fast

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _islower - check the code
+* @c: our ain int
+*  
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
