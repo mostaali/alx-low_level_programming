80cfa3b33f24693fc2d429407d5968c27b09fd21
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-12 13:30:24 -0700

    done fast

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,11 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i); /*The iteration thus the loop will never stop*/
-	}
+	/*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
