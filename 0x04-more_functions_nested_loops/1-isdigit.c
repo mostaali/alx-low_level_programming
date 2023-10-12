fcbe23ee25e151e2fb1d5fe9acf1c1cd92f2f008
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-11 23:08:06 -0700

    done fast

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _isupper - checks if a character is uppercase.
+* @c: The character to be checked.
+*
+* Return: 1 if c is uppercase, 0 otherwise.
+*/
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
