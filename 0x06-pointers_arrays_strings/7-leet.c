8578aaa006c81bc2d4a5dab796d5643c7bb434a8
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-18 14:44:42 -0700

    done fast

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,29 @@
 #include "main.h"
+
 /**
- * *string_toupper - changes all lowercase to uppercase.
+ * *leet - encodes a string into 1337
  * @str: our pointer to char
  * Return: pointer to the result
  */
+char *leet(char *str)
+{
+	char a[] = "43071";
+	char c[] = "aeotl";
+	char d[] = "AEOTL";
+	int b;
+	char *z = str;
 
-char *leet(char *str) {
-    char *p = str;
+	while (*str)
+	{
+		for (b = 0; b < 5; b++)
+		{
+			if (*str == c[b] || *str == d[b])
+			{
+				*str = a[b];
+			}
+		}
+		str++;
+	}
 
-    while (*p) {
-        char c = *p;
-        // Use a single if statement to check for the characters to be replaced
-        if (c == 'a' || c == 'A')
-            *p = '4';
-        else if (c == 'e' || c == 'E')
-            *p = '3';
-        else if (c == 'o' || c == 'O')
-            *p = '0';
-        else if (c == 't' || c == 'T')
-            *p = '7';
-        else if (c == 'l' || c == 'L')
-            *p = '1';
-        p++;
-    }
-
-	return str;
+	return (z);
 }
