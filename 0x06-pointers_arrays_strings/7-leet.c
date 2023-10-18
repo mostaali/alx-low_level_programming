6e6987b613015327a96443851f8692088dec2b14
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-18 14:12:36 -0700

    done fast

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,23 +6,23 @@
  */
 
 char *leet(char *str) {
-	char *p = str; // Create a pointer to the beginning of the string
+    char *p = str;
 
-	while (*p) {
-	char c = *p; // Get the current character
-	// Use a single if statement to check for the characters to be replaced
-	if (c == 'a' || c == 'A')
-		*p = '4';
-	else if (c == 'e' || c == 'E')
-		*p = '3';
-	else if (c == 'o' || c == 'O')
-		*p = '0';
-	else if (c == 't' || c == 'T')
-		*p = '7';
-	else if (c == 'l' || c == 'L')
-		*p = '1';
-	p++; // Move to the next character in the string
-	}
+    while (*p) {
+        char c = *p;
+        // Use a single if statement to check for the characters to be replaced
+        if (c == 'a' || c == 'A')
+            *p = '4';
+        else if (c == 'e' || c == 'E')
+            *p = '3';
+        else if (c == 'o' || c == 'O')
+            *p = '0';
+        else if (c == 't' || c == 'T')
+            *p = '7';
+        else if (c == 'l' || c == 'L')
+            *p = '1';
+        p++;
+    }
 
 	return str;
 }
