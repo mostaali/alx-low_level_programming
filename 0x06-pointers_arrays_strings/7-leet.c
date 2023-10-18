0fd7e898f9c0e2458729295125c34d97e3087bfd
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-18 13:58:19 -0700

    done fast

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,23 @@
-#include "main.h"
+#include<main.h>
 
-char *leet(char *str)
-{
-    char *ptr = str;
-    char *letters = "aAeEoOtTlL"; // Letters to be replaced
-    char *leet_code = "44370171"; // Corresponding "1337" code for the letters
+char *leet(char *str) {
+    char *p = str; // Create a pointer to the beginning of the string
 
-    while (*str) {
-        char *letter = letters;
-        char *code = leet_code;
-        int replace = 0;
-
-        // Check if the current character needs to be replaced
-        while (*letter) {
-            if (*str == *letter) {
-                *str = *code;
-                replace = 1;
-                break;
-            }
-            letter++;
-            code++;
-        }
-
-        if (!replace) {
-            str++; // Move to the next character
-        }
+    while (*p) {
+        char c = *p; // Get the current character
+        // Use a single if statement to check for the characters to be replaced
+        if (c == 'a' || c == 'A')
+            *p = '4';
+        else if (c == 'e' || c == 'E')
+            *p = '3';
+        else if (c == 'o' || c == 'O')
+            *p = '0';
+        else if (c == 't' || c == 'T')
+            *p = '7';
+        else if (c == 'l' || c == 'L')
+            *p = '1';
+        p++; // Move to the next character in the string
     }
 
-	return ptr;
+    return str;
 }
