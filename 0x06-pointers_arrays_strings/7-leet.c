df7ba65cf839aec2c9a9e1bb3f4786a486b5147a
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-18 14:01:42 -0700

    done fast

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,28 @@
 #include<main.h>
+/**
+ * *string_toupper - changes all lowercase to uppercase.
+ * @str: our pointer to char
+ * Return: pointer to the result
+ */
 
 char *leet(char *str) {
-    char *p = str; // Create a pointer to the beginning of the string
+	char *p = str; // Create a pointer to the beginning of the string
 
-    while (*p) {
-        char c = *p; // Get the current character
-        // Use a single if statement to check for the characters to be replaced
-        if (c == 'a' || c == 'A')
-            *p = '4';
-        else if (c == 'e' || c == 'E')
-            *p = '3';
-        else if (c == 'o' || c == 'O')
-            *p = '0';
-        else if (c == 't' || c == 'T')
-            *p = '7';
-        else if (c == 'l' || c == 'L')
-            *p = '1';
-        p++; // Move to the next character in the string
-    }
+	while (*p) {
+	char c = *p; // Get the current character
+	// Use a single if statement to check for the characters to be replaced
+	if (c == 'a' || c == 'A')
+		*p = '4';
+	else if (c == 'e' || c == 'E')
+		*p = '3';
+	else if (c == 'o' || c == 'O')
+		*p = '0';
+	else if (c == 't' || c == 'T')
+		*p = '7';
+	else if (c == 'l' || c == 'L')
+		*p = '1';
+	p++; // Move to the next character in the string
+	}
 
-    return str;
+	return str;
 }
