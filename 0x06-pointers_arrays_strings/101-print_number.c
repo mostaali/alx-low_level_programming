3c62331fc8c0139d0f1881021ee2a1bbc267d78e
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-20 11:44:06 -0700

    done fast

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: our int
+ * Return: no return
+ */
+void print_number(int n)
+{
+	unsigned int a, b, c;
+
+	if (n < 0)
+	{
+		a = -1 * n;
+		_putchar('-');
+	}
+	else
+	{
+		a = n;
+	}
+	b = a;
+	c = 1;
+	while (b > 9)
+	{
+		b = b / 10;
+		c = c * 10;
+	}
+	while (c >= 1)
+	{
+		_putchar(((a / c) % 10) + '0');
+		c = c / 10;
+	}
+}
+		        
