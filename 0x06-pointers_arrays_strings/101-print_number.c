925bf3dab669b4fc9fcf1fce2bf6323d3c1c40bb
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-20 11:48:08 -0700

    done fast

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,30 +5,29 @@
  * @n: our int
  * Return: no return
  */
-
 void print_number(int n)
 {
 	unsigned int a, b, c;
 
 	if (n < 0)
 	{
-	a = -1 * n;
-	_putchar('-');
+		a = -1 * n;
+		_putchar('-');
 	}
 	else
 	{
-	a = n;
+		a = n;
 	}
 	b = a;
 	c = 1;
 	while (b > 9)
 	{
-	b = b / 10;
-	c = c * 10;
+		b = b / 10;
+		c = c * 10;
 	}
 	while (c >= 1)
 	{
-	_putchar(((a / c) % 10) + '0');
-	c = c / 10;
+		_putchar(((a / c) % 10) + '0');
+		c = c / 10;
 	}
-}	        
+}
