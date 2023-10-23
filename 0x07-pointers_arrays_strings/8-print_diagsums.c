7d1b706e4dea3bdb675bea8e9d87d2e42392a5ef
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-23 11:40:59 -0700

    done fast

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,67 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix.
- * @a: A pointer to a 2D square array of integers.
- * @size: The size of the square matrix.
+ * print_diagsums -  prints the sum of the two diagonals
+ * @a: pur main int
+ * @size: size of the array
+ * Return: no return
  */
 void print_diagsums(int *a, int size)
 {
-    int sum1 = 0;
-    int sum2 = 0;
-    int i;
+	int i = 0;
+	long int sum1 = 0;
+	long int sum2 = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum1 += *(a + i * size + i); // Sum of the main diagonal
-        sum2 += *(a + i * size + (size - 1 - i)); // Sum of the other diagonal
-    }
-
-    // Print the sum1
-    int divisor = 1000000000; // The maximum power of 10 less than a 32-bit integer
-    int num1 = sum1;
-
-    if (num1 == 0)
-    {
-        _putchar('0');
-    }
-    else
-    {
-        while (divisor > 0)
-        {
-            int digit = num1 / divisor;
-            if (digit > 0 || divisor == 1)
-            {
-                _putchar(digit + '0');
-            }
-            num1 %= divisor;
-            divisor /= 10;
-        }
-    }
-
-    _putchar(',');
-
-    // Print the sum2
-    int divisor2 = 1000000000; // The maximum power of 10 less than a 32-bit integer
-    int num2 = sum2;
-
-    if (num2 == 0)
-    {
-        _putchar('0');
-    }
-    else
-    {
-        while (divisor2 > 0)
-        {
-            int digit = num2 / divisor2;
-            if (digit > 0 || divisor2 == 1)
-            {
-                _putchar(digit + '0');
-            }
-            num2 %= divisor2;
-            divisor2 /= 10;
-        }
-    }
-
-    _putchar('\n');
+	for (i = 0; i < size * size; i++)
+	{
+		if (i % (size + 1) == 0)
+		{
+			sum1 += *(a + i);
+		}
+		if (i % (size - 1) == 0 && i != 0 && i < (size * size) - 1)
+		{
+			sum2 += *(a + i);
+		}
+	}
+	printf("%ld, %ld\n", sum1, sum2);
 }
