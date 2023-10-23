246388171bff8e693f230218741750c7c58a579e
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-23 09:26:42 -0700

    done fast

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,5 +17,47 @@ void print_diagsums(int *a, int size)
         sum2 += a[i * size + (size - 1 - i)];
     }
 
-    printf("%d, %d\n", sum1, sum2);
+    char buffer[12]; // Assuming int max is 10 digits, plus a comma and space.
+    int num1 = sum1;
+    int num2 = sum2;
+
+    if (num1 == 0) {
+        _putchar('0');
+    } else {
+        if (num1 < 0) {
+            _putchar('-');
+            num1 = -num1;
+        }
+        int index = 0;
+        while (num1 != 0) {
+            buffer[index] = (num1 % 10) + '0';
+            num1 /= 10;
+            index++;
+        }
+        for (; index >= 0; index--) {
+            _putchar(buffer[index]);
+        }
+    }
+
+    _putchar(',');
+
+    if (num2 == 0) {
+        _putchar('0');
+    } else {
+        if (num2 < 0) {
+            _putchar('-');
+            num2 = -num2;
+        }
+        int index = 0;
+        while (num2 != 0) {
+            buffer[index] = (num2 % 10) + '0';
+            num2 /= 10;
+            index++;
+        }
+        for (; index >= 0; index--) {
+            _putchar(buffer[index]);
+        }
+    }
+
+    _putchar('\n');
 }
