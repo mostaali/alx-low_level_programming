433c6b2d4e6dc4ef74a271f36995389d6576c355
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-25 10:41:10 -0700

    done fast

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,8 +16,8 @@ int wildcmp(char *s1, char *s2)
 	return (*s2 == '\0');
 	}
 	if (*s2 == '\0')
-        return (*s1 == '\0');
-    
+	return (*s1 == '\0');
+
 	if (*s2 == '*')
 	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
