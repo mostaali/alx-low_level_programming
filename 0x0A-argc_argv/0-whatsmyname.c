011a545ce88541a77d79c761a41ada427b0b0ba8
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-27 13:56:15 -0700

    done fast

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - main function
+ * @argc: numbers of stuff added
+ * @argv: the things added
+ *
+ * Return: always 0 SUCCESS
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
