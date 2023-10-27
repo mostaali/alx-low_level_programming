fd1e58688e947eed6cbf870833ef774d785d9754
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-27 14:19:20 -0700

    done fast

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
