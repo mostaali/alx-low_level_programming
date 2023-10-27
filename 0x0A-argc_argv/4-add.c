aefd943217f45670c1b960ec75b59b834bcf4d37
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-27 15:06:44 -0700

    done fast

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,48 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-int _putchar(char c) {
-    return write(1, &c, 1);
-}
-
-int is_digit(char *str) {
+int is_digit_string(char *str) {
     while (*str) {
         if (*str < '0' || *str > '9') {
-            return 0;
+            return 0; // Not a digit
         }
         str++;
     }
-    return 1;
+    return 1; // All characters are digits
 }
 
-int add_numbers(int argc, char *argv[]) {
-    int sum = 0;
+int main(int argc, char *argv[])
+{
+    int i, sum = 0;
 
-    if (argc == 1) {
-        _putchar('0');
-        _putchar('\n');
+    if (argc == 1)
+    {
+        printf("0\n");
         return 0;
     }
-
-    for (int i = 1; i < argc; i++) {
-        if (is_digit(argv[i])) {
+    for (i = 1; i < argc; i++)
+    {
+        if (is_digit_string(argv[i])) {
             sum += atoi(argv[i]);
         } else {
-            _putchar('E');
-            _putchar('r');
-            _putchar('r');
-            _putchar('o');
-            _putchar('r');
-            _putchar('\n');
+            printf("Error\n");
             return 1;
         }
     }
-
     printf("%d\n", sum);
-    return 0;
-}
 
-int main(int argc, char *argv[]) {
-    return add_numbers(argc, argv);
+    return 0;
 }
