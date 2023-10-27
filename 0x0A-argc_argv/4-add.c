14bd616915510c8af912d5ee6885137439b65666
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-27 14:39:19 -0700

    done fast

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+int is_digit(char *str) {
+    while (*str) {
+        if (*str < '0' || *str > '9') {
+            return 0;
+        }
+        str++;
+    }
+    return 1;
+}
+
+int add_numbers(int argc, char *argv[]) {
+    int sum = 0;
+
+    if (argc == 1) {
+        _putchar('0');
+        _putchar('\n');
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (is_digit(argv[i])) {
+            sum += atoi(argv[i]);
+        } else {
+            printf("Error\n");
+            return 1;
+        }
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    return add_numbers(argc, argv);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int is_digit(char *str);
+int add_numbers(int argc, char *argv[]);
+
+#endif /* MAIN_H */
