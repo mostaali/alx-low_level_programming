7683a4f274be811128aff3ab3f0f8a929b063eaa
Mostafa <mostafa.ali.ahmed@outlook.com>
2023-10-30 14:41:16 -0700

    done fast

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,18 +8,15 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	if (size == 0) {
+	if (size == 0)
 	return (NULL);
-	}
 
 	array = malloc(size * sizeof(char));
-	if (array == NULL) {
+	if (array == NULL)
 	return (NULL);
-	}
 
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
 	array[i] = c;
-	}
 
 	return (array);
 }
