2c409cd4264f99287091ecfae324a6d1ff43a0b0
Mostaaaaa <mostafa.ali.ahmed@gmail.com>
2023-10-30 17:02:06 -0700

    done fast

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,11 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 	s2 = "";
 
-	for (length1 = 0; s1[length1] != '\0'; length1++);
+	for (length1 = 0; s1[length1] != '\0'; length1++)
+	;
 
-	for (length2 = 0; s2[length2] != '\0'; length2++);
+	for (length2 = 0; s2[length2] != '\0'; length2++)
+	;
 
 	concatenated = (char *)malloc(sizeof(char) * (length1 + length2 + 1));
 
