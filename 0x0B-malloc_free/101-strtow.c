1968c1bf10897951112bf1cb21f513fe78fdb682
Mostaaaaa <mostafa.ali.ahmed@gmail.com>
2023-10-30 17:24:04 -0700

    done fast

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,16 +8,15 @@ void copy_words(char **words, char *str, int word_count);
 char **strtow(char *str)
 {
     int word_count;
-    char **words;
 
-    if (str == NULL)
+    if (str == NULL || *str == '\0')
         return NULL;
 
     word_count = count_words(str);
     if (word_count == 0)
         return NULL;
 
-    words = allocate_memory(word_count);
+    char **words = allocate_memory(word_count);
     if (words == NULL)
         return NULL;
 
@@ -28,24 +27,68 @@ char **strtow(char *str)
 int count_words(char *str)
 {
     int count = 0;
-    // Implement your counting logic here.
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+            count++;
+    }
+
     return count;
 }
 
 char **allocate_memory(int word_count)
 {
-    char **words = NULL;
-    // Allocate memory for the array of words.
+    char **words;
+    int i;
+
+    words = (char **)malloc(sizeof(char *) * (word_count + 1));
+    if (words == NULL)
+        return NULL;
+
+    for (i = 0; i < word_count; i++)
+    {
+        words[i] = NULL;
+    }
+
     return words;
 }
 
 void free_memory(char **words, int word_count)
 {
-    // Free the memory for the words array.
+    int i;
+
+    for (i = 0; i < word_count; i++)
+    {
+        free(words[i]);
+    }
+    free(words);
 }
 
 void copy_words(char **words, char *str, int word_count)
 {
-    // Copy words from the string into the words array.
-    // Implement the copying logic.
+    int i, j, k;
+
+    for (i = 0, k = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+        {
+            for (j = i; str[j] != '\0' && str[j] != ' '; j++)
+                ;
+            words[k] = (char *)malloc(sizeof(char) * (j - i + 1));
+
+            if (words[k] == NULL)
+            {
+                free_memory(words, word_count);
+                break;
+            }
+
+            for (k = 0; i < j; i++, k++)
+                words[k][k] = str[i];
+            words[k][k] = '\0';
+            k++;
+        }
+    }
+    words[k] = NULL;
 }
